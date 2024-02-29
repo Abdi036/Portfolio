54c820bd7c29f07d2f83f4186f0f367af3ed20a5
Abdi kumela <abdikumela@036gmail.com>
2024-02-29 08:57:29 -0800

    check

---
+++ index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const tabTitles = document.querySelector(".tab-titles");
+const tabLinks = document.querySelectorAll(".tab-links");
+const tabContents = document.querySelectorAll(".tab-contents");
+const openBar = document.querySelector(".fa-bars");
+const sideBar = document.querySelector(".side-bar");
+const closeBar = document.querySelector(".fa-circle-xmark");
+const navContainer = document.querySelector(".nav-container");
+
+tabTitles.addEventListener("click", function (event) {
+  if (!event.target || !event.target.classList.contains("tab-links")) {
+    return;
+  }
+
+  for (let tabLink of tabLinks) {
+    tabLink.classList.remove("active");
+  }
+
+  for (let tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+
+  event.target.classList.add("active");
+
+  const index = Array.from(tabLinks).indexOf(event.target);
+
+  tabContents[index].classList.add("active-tab");
+});
+
+openBar.addEventListener("click", function () {
+  sideBar.style.right = "0";
+});
+
+closeBar.addEventListener("click", function () {
+  sideBar.style.right = "-200px";
+});
+
+// implementing navigation bar
+const scrollDistance = 450;
+
+window.addEventListener("scroll", function () {
+  const scrollTop = document.documentElement.scrollTop;
+
+  if (scrollTop > scrollDistance) {
+    navContainer.style.top = "0";
+  } else {
+    navContainer.style.top = "-100px";
+  }
+});
